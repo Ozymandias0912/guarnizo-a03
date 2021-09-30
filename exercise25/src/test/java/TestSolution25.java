2e8112797473384cc46269b0d589357b8eb5804d
Juan <jdavid29a@gmail.com>
2021-09-30 12:03:27 -0400

    added test code and main code for exercise25

---
+++ exercise25/src/test/java/TestSolution25.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class TestSolution25 {
+
+
+
+    @Test
+    void passwordStrength()
+    {
+
+        assertEquals(3, baseline.Solution25.passwordStrength("Guarnizo12"));
+    }
+}
