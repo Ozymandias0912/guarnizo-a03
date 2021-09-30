28118a430cdc39a8da4867d8b83a0cc826fa2b57
Juan <jdavid29a@gmail.com>
2021-09-30 08:29:22 -0400

    added pseudocode exercise25

---
+++ exercise25/src/main/java/baseline/Solution25.java
@@ -0,0 +1,57 @@
+package baseline;
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Juan Guarnizo
+ */
+/*
+*       Password Strength Indicator
+*
+*       //main function
+*
+*   call passwordStrength function
+*
+*
+*       //end main function
+*
+*
+*   //begin passwordStrength function
+*
+*
+*
+*       //create password variable
+*
+*       //scan a string from the user and save it as the password
+*
+*       //use if statement
+*       //if the password contains only numbers and is fewer than eight characters.
+*           //return case 1
+*       //if the password contains only letters and is fewer than eight characters.
+*           //return case 2
+*       //if the password contains letters and at least one number and is at least eight characters.
+*           //return case 3
+*       //if the password contains letters, numbers, and special characters and is at least eight characters.
+*           //return case 4
+*       //if the password does not meet any of these rules.
+*           //return case 5
+*
+*
+*       //Use switch statement
+*       //create password strength variable
+*       //case 1 :  password strength variable is very weak
+*       //case 2 :  password strength variable is weak
+*       //case 3 :  password strength variable is strong
+*       //case 4 :  password strength variable is very strong
+*       //default:  password strength variable is unknown strength
+*
+*   //print: The password 'password' is a 'password strength variable' password.
+*
+*  //end passwordStrength function
+*
+*
+*
+*
+*
+* */
+public class Solution25 {
+}
