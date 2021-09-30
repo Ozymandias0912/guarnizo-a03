ada596f93585a34073eea4137534dac7706451b3
Juan <jdavid29a@gmail.com>
2021-09-30 17:51:09 -0400

    added exercise27 pseudocode

---
+++ exercise27/src/main/java/baseline/Solution27.java
@@ -0,0 +1,75 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Juan Guarnizo
+ */
+/*
+*       Validating Inputs
+*
+*   //begin solution27 class
+*       //begin main
+*           create firstname
+*           create lastname
+*           create ID
+*           create zipcode
+*
+*           scanInfo(firstname, lastname, ID, zipcode)
+*
+*
+*           employee(firstname, lastname, ID, zipcode)
+*
+*
+*       //end main
+*      //scanInfo(firstname, lastname, ID, zipcode){
+*       scan firstname
+*       scan lastname
+*       scan ID
+*       scan zipcode
+*       }
+*      //end scanInfo
+*   //end solution27 class
+*
+*   //begin employee class
+*
+*       firstname
+*       lastname
+*       ID
+*       zipcode
+*
+*   void employee(firstname, lastname, ID, zipcode){
+*       if(checkFirstName)
+*           assign firstname
+*       if(checkLastName)
+*           assign lastname
+*       if(checkID)
+*           assign ID
+*       if(checkZipcode)
+*           assign zipcode
+*   }
+*
+*   boolean checkFirstName(firstname){
+*       if it follows the rules, return true
+*       else print a warning, return false
+* }
+*   boolean checkFirstName(lastname){
+*       if it follows the rules, return true
+*       else print a warning, return false
+* }
+*   boolean checkFirstName(ID){
+*    if it follows the rules, return true
+*   else print a warning, return false
+* }
+*   boolean checkFirstName(Zipcode){
+*    if it follows the rules, return true
+*    else print a warning, return false
+*
+* }
+*
+*   //end employee class
+*
+*
+* */
+package baseline;
+
+public class Solution27 {
+
+}
