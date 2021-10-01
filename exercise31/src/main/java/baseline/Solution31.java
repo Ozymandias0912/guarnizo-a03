be3a2153a90aa675aef5708c89dcce8b34dd07d3
Juan <jdavid29a@gmail.com>
2021-10-01 19:10:54 -0400

    pseudocode exercise 31

---
+++ exercise31/src/main/java/baseline/Solution31.java
@@ -0,0 +1,27 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 first_name last_name
+ */
+package baseline;
+//fitness class
+    //age
+    //restingPulse
+//constructor method
+
+//TargetHeartRate method
+//printRow method
+//print method
+
+    //uses TargetHeartRate method and printRow method
+//end fitness class
+
+//solution31 class
+    //main
+        //call constructor
+        //call print method
+
+    //end main
+
+//end solution31 class
+public class Solution31 {
+}
