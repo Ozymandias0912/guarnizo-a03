d66d8eb45160e742230e2083ae8e78a10f9a184b
Juan <jdavid29a@gmail.com>
2021-10-01 14:17:10 -0400

    pseudocode exercise 30

---
+++ exercise30/src/main/java/baseline/Solution30.java
@@ -0,0 +1,36 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Juan Guarnizo
+ */
+/*
+*   Pseudocode
+*
+* //class table
+* int rows
+* int columns
+*
+* table(row, columns)
+*
+* fill row()
+*
+*
+*
+*
+* //end class table
+*
+* //main
+* create table object
+*
+* call fill row method in a for loop
+*
+* print items in the array using for loop
+* //end main
+*
+*
+*
+* */
+package baseline;
+
+public class Solution30 {
+
+}
