b683400b255a43da26d3568a6603f04297ae7f02
Juan <jdavid29a@gmail.com>
2021-10-01 11:39:42 -0400

    added exercise 28 pseudocode

---
+++ exercise28/src/main/java/baseline/Solution28.java
@@ -0,0 +1,27 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Juan Guarnizo
+ */
+/*
+* Pseudocode
+*
+*
+* create scanner variable
+*
+* create sum variable = 0
+*
+* for loop
+* prompt for number
+* scan number
+*
+* add number to sum
+* end for loop
+*
+* prompt answer
+*
+*
+* */
+package baseline;
+
+public class Solution28 {
+}
