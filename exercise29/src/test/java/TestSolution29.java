15fdfc8caa240e72c4584bcf557faafda8cd32df
Juan <jdavid29a@gmail.com>
2021-10-01 14:07:50 -0400

    added test

---
+++ exercise29/src/test/java/TestSolution29.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestSolution29 {
+    @Test
+    void isDigit(){
+        assertEquals(false, baseline.Solution29.isDigit('a')) ;
+    }
+}
