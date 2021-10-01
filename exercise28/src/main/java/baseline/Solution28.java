00046a252ccdcfb5b0d8e9eaeea1659642fb9991
Juan <jdavid29a@gmail.com>
2021-10-01 12:30:13 -0400

    added main code

--- exercise28/src/main/java/baseline/Solution28.java
+++ exercise28/src/main/java/baseline/Solution28.java
@@ -3,25 +3,33 @@
  *  Copyright 2021 Juan Guarnizo
  */
 /*
-* Pseudocode
 *
-*
-* create scanner variable
-*
-* create sum variable = 0
-*
-* for loop
-* prompt for number
-* scan number
-*
-* add number to sum
-* end for loop
-*
-* prompt answer
 *
 *
 * */
 package baseline;
 
+import java.util.Scanner;
+
 public class Solution28 {
-}
+    public static void main(String[] args) {
+
+    // create scanner variable
+    Scanner input = new Scanner(System.in);
+    //create sum variable = 0
+    int sum = 0;
+
+    // for loop
+        for(int i = 0; i < 5; i++){
+            // prompt for number
+            System.out.println("Enter an integer number: ");
+            // scan number
+            // add number to sum
+            sum += input.nextInt();
+
+        }//end for loop
+
+    // prompt answer
+    System.out.printf("The sum of the numbers is %d", sum);
+    }//end main
+}//end class
