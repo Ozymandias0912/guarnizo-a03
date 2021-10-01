b48c282f41ea552864ec3a049fa01d195423bd7c
Juan <jdavid29a@gmail.com>
2021-10-01 13:12:54 -0400

    added exercise 29 pseudocode

---
+++ exercise29/src/main/java/baseline/Solution29.java
@@ -0,0 +1,59 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Juan Guarnizo
+ */
+/*  Pseudocode
+
+    //main
+        create scanner variable
+        create rate variable
+
+        Prompt for the rate of return
+        save the input in rate
+
+
+    //end main
+
+    //calculateDoublingTime method
+        create scanner variable
+        create rate variable
+        create years variable
+
+        Prompt for the rate of return
+        save the input in rate
+
+        while !checkInput(rate)
+
+        prompt  invalid input
+        //end while loop
+
+
+        parse string to double
+        years = 72 / r
+
+        print years
+
+    //end calculateDoublingTime method
+
+    //boolean checkInput method(rate)
+        if rate <= 0
+        return false
+        //for loop
+        if rate[i] !isDigit || !'.'
+            return false
+        else
+            return true
+        //end for loop
+    //end checkInput method
+
+
+    //isDigit method
+    ...
+    //end  isDigit method
+* */
+
+
+package baseline;
+
+public class Solution29 {
+}
