b045248be387850c62679fa6c46ea163d330288a
Juan <jdavid29a@gmail.com>
2021-10-02 11:54:56 -0400

    added pseudocode ex32

---
+++ exercise32/src/main/java/baseline/Solution32.java
@@ -0,0 +1,70 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Juan Guarnizo
+ */
+
+package baseline;
+/*
+*   Guess the Number Game
+*
+* //gameSet class
+* //size
+* //int array
+*
+* //void constructor easy method(10, array)
+*
+* //void constructor medium method(100, array)
+*
+* //void constructor hard method(1000, array)
+*
+* //boolean guess method()
+*
+*   //uses checkInput method()
+*   //uses hint method()
+*
+* //boolean checkInput method(input)
+*
+* //void hint method(input)
+*
+* //int pickNumber method()
+*
+*
+*
+*
+* //end gameSet class
+*
+* //solution38 class
+*   //main
+*           //call startGame method()
+*           //print do you want to play again?
+*           //scan input in answer variable
+*       //if(answer = yes)
+*           //call startGame()
+*           //if(answer = no)
+*               //exit program
+*           //else
+*               //print do you want to play again?
+*   //end main
+*  //startGame method()
+ *        //create scanner
+ *        //create guess counter
+ *       //ask the user for difficulty
+ *       //call the right constructor
+ *       //call pickNumber method()
+ *       //while(!guess method)
+ *           //call guess method
+ *           //counter ++
+ *       //if(guess method)
+ *           //counter ++
+ *           //print you got it in "counter" guesses!
+ *
+ *   //end startGame method()
+*
+* //end solution38 class
+*
+*
+*
+*
+* */
+public class Solution32 {
+}
